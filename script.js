12912c848a113f416c065301c3699588604ca877
Potato0119 <502ender502@gmail.com>
2024-03-02 23:16:36 +0800

    1.0.0

--- script.js
+++ script.js
@@ -1,40 +1,93 @@
+function isMobileDevice() {
+  let mobileDevices = [
+    "Android",
+    "webOS",
+    "iPhone",
+    "iPad",
+    "iPod",
+    "BlackBerry",
+    "Windows Phone",
+  ];
+  for (var i = 0; i < mobileDevices.length; i++) {
+    if (navigator.userAgent.match(mobileDevices[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // 起始動畫
 const str = "<p>嗨 很高興認識你<br>我叫馬鈴薯 英文是Potato0119<br>你可以叫我馬鈴薯 或 破鐵頭 或 Potato<br>如果有靜態網站設計的需求可以找我<br>從編寫程式到網站架設幫您一條龍服務</p>"
 const strFinal = "嗨 很高興認識你<br>我叫馬鈴薯 英文是Potato0119<br>你可以叫我馬鈴薯 或 破鐵頭 或 Potato<br>如果有靜態網站設計的需求可以找我<br>從編寫程式到網站架設幫您一條龍服務"
 const potato = "Potato0119's Web"
 function start(){
-  setTimeout(() => {
-    document.querySelector(".title1").style = "color: #eab532;"
-    document.querySelector(".title2").style = "color: #9154d2;"
-  }, 1000)
-
-  setTimeout(() => {
-    const sub = document.querySelector(".sub1")
-    i=1
-    var loop1 = setInterval(() => {
-      sub.innerHTML = potato.slice(0,i)
-      i++
-      if (i == potato.length+2){
-        clearInterval(loop1)
-        setTimeout(() => {sub.style = "text-shadow: 0 0 1vw #eab532;"}, 500)
-      }
-    }, 50);
-
+  if (isMobileDevice()) {
     setTimeout(() => {
-      const typing = document.querySelector(".main p")
-      j=1
-      var loop2 = setInterval(() => {
-        typing.textContent = str.slice(0,j)
-        j++
-        if (j == str.length+2){
-          clearInterval(loop2)
-          setTimeout(() => {typing.innerHTML = strFinal}, 500)
-          setTimeout(() => {typing.style = "text-shadow: 0 0 1vw #9154d2;"}, 1000)
+      if (window.scrollY <= window.innerHeight){
+          back()
+      }
+    }, 1)
+    setTimeout(() => {
+      const sub = document.querySelector(".sub1")
+      i=1
+      var loop1 = setInterval(() => {
+        sub.innerHTML = potato.slice(0,i)
+        i++
+        if (i == potato.length+2){
+          clearInterval(loop1)
+          setTimeout(() => {sub.style = "text-shadow: 0 0 1vw #00c8ff;"}, 500)
         }
-    }, 50);
+      }, 50);
+      setTimeout(() => {
+        const typing = document.querySelector(".main p")
+        j=1
+        var loop2 = setInterval(() => {
+          typing.textContent = str.slice(0,j)
+          j++
+          if (j == str.length+2){
+            clearInterval(loop2)
+            setTimeout(() => {typing.innerHTML = strFinal}, 500)
+            setTimeout(() => {typing.style = "text-shadow: 0 0 1vw #00c8ff;"}, 1000)
+          }
+      }, 50);
+      }, 2000)
+    }, 1000)
+  }else{
+    setTimeout(() => {
+      if (window.scrollY <= window.innerHeight){
+          back()
+      }
+    }, 1)
+    setTimeout(() => {
+      document.querySelector(".title1").style = "color: #eab532;"
+      document.querySelector(".title2").style = "color: #9154d2;"
+    }, 1000)
+    setTimeout(() => {
+      const sub = document.querySelector(".sub1")
+      i=1
+      var loop1 = setInterval(() => {
+        sub.innerHTML = potato.slice(0,i)
+        i++
+        if (i == potato.length+2){
+          clearInterval(loop1)
+          setTimeout(() => {sub.style = "text-shadow: 0 0 1vw #eab532;"}, 500)
+        }
+      }, 50);
+      setTimeout(() => {
+        const typing = document.querySelector(".main p")
+        j=1
+        var loop2 = setInterval(() => {
+          typing.textContent = str.slice(0,j)
+          j++
+          if (j == str.length+2){
+            clearInterval(loop2)
+            setTimeout(() => {typing.innerHTML = strFinal}, 500)
+            setTimeout(() => {typing.style = "text-shadow: 0 0 1vw #9154d2;"}, 1000)
+          }
+      }, 50);
+      }, 2000)
     }, 2000)
-
-  }, 2000)
+  }
 }
 
 // Discord
@@ -50,46 +103,47 @@ function discord(){
 // Shop
 const sblock1  = document.getElementById('sblock1');
 const sub1 = document.getElementById('sub1');
-sblock1.addEventListener('mouseenter', function() {
-    sblock1.style.height="31vw";
-    sub1.style.opacity="1"
-});
-sblock1.addEventListener('mouseleave', function() {
-    sblock1.style.height="calc((14vw * 600 / 500) + 4vw)";
-    sub1.style.opacity="0"
-});
-
 const sblock2  = document.getElementById('sblock2');
 const sub2 = document.getElementById('sub2');
-sblock2.addEventListener('mouseenter', function() {
-    sblock2.style.height="28vw";
-    sub2.style.opacity="1"
-});
-sblock2.addEventListener('mouseleave', function() {
-    sblock2.style.height="calc((14vw * 600 / 500) + 4vw)";
-    sub2.style.opacity="0"
-});
-
 const sblock3  = document.getElementById('sblock3');
 const sub3 = document.getElementById('sub3');
-sblock3.addEventListener('mouseenter', function() {
-    sblock3.style.height="28vw";
-    sub3.style.opacity="1"
-});
-sblock3.addEventListener('mouseleave', function() {
-    sblock3.style.height="calc((14vw * 600 / 500) + 4vw)";
-    sub3.style.opacity="0"
-});
-
 const sblock4 = document.getElementById('sblock4');
 const sub4 = document.getElementById('sub4');
+
+sblock1.addEventListener('mouseenter', function() {
+  sblock1.style.height="31vh";
+  sub1.style.opacity="1"
+});
+sblock1.addEventListener('mouseleave', function() {
+  sblock1.style.height="calc((14vh * 600 / 500) + 4vh)";
+  sub1.style.opacity="0"
+});
+
+sblock2.addEventListener('mouseenter', function() {
+  sblock2.style.height="28vh";
+  sub2.style.opacity="1"
+});
+sblock2.addEventListener('mouseleave', function() {
+  sblock2.style.height="calc((14vh * 600 / 500) + 4vh)";
+  sub2.style.opacity="0"
+});
+
+sblock3.addEventListener('mouseenter', function() {
+  sblock3.style.height="28vh";
+  sub3.style.opacity="1"
+});
+sblock3.addEventListener('mouseleave', function() {
+  sblock3.style.height="calc((14vh * 600 / 500) + 4vh)";
+  sub3.style.opacity="0"
+});
+
 sblock4.addEventListener('mouseenter', function() {
-    sblock4.style.height="24.5vw";
-    sub4.style.opacity="1"
+  sblock4.style.height="24.5vh";
+  sub4.style.opacity="1"
 });
 sblock4.addEventListener('mouseleave', function() {
-    sblock4.style.height="calc((14vw * 600 / 500) + 4vw)";
-    sub4.style.opacity="0"
+  sblock4.style.height="calc((14vh * 600 / 500) + 4vh)";
+  sub4.style.opacity="0"
 });
 
 // header
@@ -109,20 +163,25 @@ function home(){
   });
 }
 function about(){
-  window.scrollTo({
-    top: window.innerHeight,
+  const aboutPage = document.getElementById('about');
+  aboutPage.scrollIntoView({
     behavior: 'smooth'
   });
 }
 function project(){
-  window.scrollTo({
-    top: window.innerHeight * 2,
+  const projectPage = document.getElementById('project');
+  projectPage.scrollIntoView({
     behavior: 'smooth'
   });
 }
 function shop(){
-  window.scrollTo({
-    top: window.innerHeight * 3,
+  const shopPage = document.getElementById('shop');
+  shopPage.scrollIntoView({
     behavior: 'smooth'
+  });
+}
+function back(){
+  window.scrollTo({
+    top: 0
   });
 }
