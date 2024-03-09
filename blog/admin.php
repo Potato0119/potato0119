8ecb8d416c31801cd1d81ee981dc955cfbb11533
Potato0119 <502ender502@gmail.com>
2024-03-09 15:02:06 +0800

    1.2.0

---
+++ blog/admin.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+<head>
+  <!-- meta, title -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <title>Admin!</title>
+
+  <!-- icon, robots-->
+  <link type="image/x-icon" href="./../favicon.ico" rel="icon" />
+  <link type="image/x-icon" href="./../favicon.ico" rel="bookmark" />
+  <link data-default-icon="./../favicon.ico" data-badged-icon="./../favicon.ico" rel="shortcut icon" href="./../favicon.ico"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap" rel="stylesheet">
+  <style>
+  body {
+    font-family: "Noto Sans TC", sans-serif;
+  }
+  .blogStyle{
+    background-color: #faebd7;
+    width: 80vw;
+    height: 8vw;
+    margin-left: 10vw;
+    border-radius: 1vw;
+    margin-top: 1vw;
+  }
+  .blogTitle{
+      position: absolute;
+      width: 65vw;
+      height: 2vw;
+      display: flex;
+      align-items: center;
+  }
+  .blogTitle input{
+      font-size: 1.5vw;
+      margin-left: 1em;
+      font-weight: bold;
+      font-family: "Noto Sans TC", sans-serif;
+  }
+  .blogDate{
+      position: relative;
+      left:65vw;
+      width: 15vw;
+      height: 2vw;
+      display: flex;
+      align-items: center;
+      justify-content: end;
+  }
+  .blogDate input{
+      font-size: 1vw;
+      margin-right: 1em;
+      font-weight: bold;
+      font-family: "Noto Sans TC", sans-serif;
+  }
+  .blogContent{
+      position: absolute;
+      width: 80vw;
+      height: 6vw;
+  }
+  .blogContent textarea{
+      font-size: 1vw;
+      margin-left: 1.5vw;
+      margin-right: 1.5vw;
+      margin-top: 1em;
+      width:77vw;
+      height:3.6vw;
+      line-height:1.2em;
+      font-family: "Noto Sans TC", sans-serif;
+  }
+  .sso,.export{
+    margin-top:1vw;
+    margin-left:11vw;
+    font-family: "Noto Sans TC", sans-serif;
+  }
+  </style>
+</head>
+<body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+        <div class="blogStyle">
+          <div class="blogTitle"><p><input name="title"></p></div>
+          <div class="blogDate"><p><input type="date" name="date"></p></div>
+          <div class="blogContent"><p><textarea name="content"></textarea></p></div>
+        </div>
+        <input class="sso" name="sso">
+        <input type="submit" name="submit" value="提交">
+    </form>
+    <?php
+      $SQLservername = "localhost";
+      $SQLusername = "root";
+      $SQLpassword = "";
+      $SQLdbname = "potato";
+      $conn = new mySqli($SQLservername, $SQLusername, $SQLpassword, $SQLdbname);
+      if ($conn->connect_error) {
+          die("<script>console.log('" . "MySQL資料庫連線失敗: " . $conn->connect_error . "')</script>");
+      } else{
+          echo "<script>console.log('MySQL資料庫連線成功')</script>";
+      }
+
+      $data = "SELECT * FROM `blog`"; 
+      $allData = $conn->query($data);
+      if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+        if ($_POST["title"] == "" or $_POST["date"] == "" or $_POST["content"] == ""){
+          echo "<p class='export'>資料為空</p>";
+        }else if ($_POST["sso"] == "19657238970119") {
+          $maxid = 0;
+          while($row = $allData->fetch_assoc()) {
+            if ($maxid < $row["id"]){
+              $maxid = $row["id"];
+            }
+          }
+          $maxid = $maxid + 1;
+          $new = '<div class="blogStyle"><div class="blogTitle"><p>' . $_POST["title"] . '</p></div><div class="blogDate"><p>' . $_POST["date"] . '</p></div><div class="blogContent"><p>' . $_POST["content"] . '</p></div></div>';
+          $create = "INSERT INTO `blog` (`content`, `title`, `id`) VALUES (?, ?, ?)";
+          $toSave = $conn->prepare($create);
+          $toSave->bind_param("ssi",$new,$_POST["title"],$maxid);
+          $toSave->execute();
+          $toSave->close();
+          echo "<p class='export'>Submitted successfully.</p>";
+          header("Location: admin.php");
+          exit;
+        }else {
+          echo "<p class='export'>Verfiy error.</p>";
+        }
+      }
+    ?>
+    <script src="./script.js"></script>
+</body>
+</html>
+
---
+++ blog/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+<head>
+  <!-- meta, title -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <title>馬鈴薯Blog | Potato0119's Blog</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./../reset.css" />
+  <link rel="stylesheet" href="./style.css" />
+
+  <!-- icon, robots-->
+  <link type="image/x-icon" href="./../favicon.ico" rel="icon" />
+  <link type="image/x-icon" href="./../favicon.ico" rel="bookmark" />
+  <link data-default-icon="./../favicon.ico" data-badged-icon="./../favicon.ico" rel="shortcut icon" href="./../favicon.ico"/>
+
+  <!-- font and other-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet"/>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+    <img class="bg" src="./../image/bg.png"/>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+        <div class="header" id="header">
+          <div class="title" id="title">
+            <img src="/image/icon.png" />
+            <p>馬鈴薯的個人網站</span></p>
+          </div>
+          <nav class="nav-pc">
+            <ul class="nav-ul">
+              <li><a href="./../index.html">首頁</a></li>
+              <li><a href="./index.php">馬鈴薯Blog</a></li>
+            </ul>
+          </nav>
+        </div>
+    </form>
+    <?php
+      $SQLservername = "localhost";
+      $SQLusername = "root";
+      $SQLpassword = "";
+      $SQLdbname = "potato";
+      $conn = new mySqli($SQLservername, $SQLusername, $SQLpassword, $SQLdbname);
+      if ($conn->connect_error) {
+          die("<script>console.log('" . "MySQL資料庫連線失敗: " . $conn->connect_error . "')</script>");
+      } else{
+          echo "<script>console.log('MySQL資料庫連線成功')</script>";
+      }
+      
+      $data = "SELECT * FROM `blog`"; 
+      $allData = $conn->query($data);
+      $contents = "";
+      while($row = $allData->fetch_assoc()) {
+        $contents = $row["content"] . $contents;
+      }
+      $blog = '<div class="blog">
+                <div class="blogHeader">
+                  <p class="headerTitle">嗨 這是我的Blog(or廢文)</p>
+                  <p class="headerContent">可能有點枯燥無聊XD 請見諒</p>
+                </div>' . $contents . '<div class="footer"><p>Copyright © 2024・馬鈴薯Potato0119</p></div></div>';
+      echo $blog;
+    ?>
+    <script src="./script.js"></script>
+</body>
+</html>
